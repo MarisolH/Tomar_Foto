3cfd233b0f292619450782eda4313b01a0a72f92
root <root@localhost.localdomain>
2018-03-16 13:54:03 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tomar_Foto
---
+++ src/main/java/com/proyecto/demo/webcam_test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyecto.demo;
+
+/**
+ *
+ * @author mhernandez
+ */
+public class webcam_test extends JFrameWindow{
+    
+    public static void main(String args[]) {
+        JFrameWindow window = new JFrameWindow();
+        window.setVisible(true);
+        
+         
+        
+    }
+    
+}
